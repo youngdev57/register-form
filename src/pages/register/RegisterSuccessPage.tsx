1da73220a9f3c8701728126f4c242587ef673ac3
Minyoung Choi <youngdev57@gmail.com>
2025-07-18 18:33:06 +0900

    feat: 리셋 기능 연결

--- src/pages/register/RegisterSuccessPage.tsx
+++ src/pages/register/RegisterSuccessPage.tsx
@@ -2,12 +2,20 @@ import { useRegisterStore } from "@/stores/registerStore";
 import { useNavigate } from "react-router-dom";
 
 export default function RegisterSuccessPage() {
-  const form = useRegisterStore((state) => state.form);
+  const { form, resetForm } = useRegisterStore((state) => ({
+    form: state.form,
+    resetForm: state.resetForm,
+  }));
 
   const navigate = useNavigate();
 
-  const handleRedirectMain = () => {
+  const handleReset = () => {
+    resetForm();
     navigate("/");
+  };
+
+  const handleLogin = () => {
+    window.location.href = "https://youngdev57.github.io/user-login";
   };
 
   return (
@@ -25,17 +33,23 @@ export default function RegisterSuccessPage() {
             님의 회원가입이
             <br /> 완료되었어요.
             <br />
-            다양한 콘텐츠를 즐겨보세요! 🎉
+            로그인 후 다양한 콘텐츠를 즐겨보세요! 🎉
           </h2>
         </div>
 
-        {/* 메인 이동 버튼 */}
-        <div>
+        {/* 버튼 영역 */}
+        <div className="flex gap-4">
           <button
-            onClick={handleRedirectMain}
+            onClick={handleReset}
+            className="w-full bg-white border border-blue-500 text-blue-500 p-2 rounded disabled:bg-gray-300 mt-6 hover:bg-gray-50 transition"
+          >
+            처음으로
+          </button>
+          <button
+            onClick={handleLogin}
             className="w-full bg-white border border-blue-500 text-blue-500 p-2 rounded disabled:bg-gray-300 mt-6 hover:bg-blue-500 hover:text-white transition"
           >
-            메인으로 이동
+            로그인
           </button>
         </div>
       </div>
